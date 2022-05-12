2194ccbb572d6f2f5821b9f1d275ed7ab106f51e
RaminQuliyev99 <98511186+RaminQuliyev99@users.noreply.github.com>
2022-05-12 10:46:02 +0400

    Add files via upload

---
+++ combat/script.js
@@ -0,0 +1,23 @@
+const { createApp } = Vue
+Vue.createApp({
+    data(){
+        return{
+            1 : "true",
+            0 : "false",
+            player: "Player",
+            mob : "Mob",
+            start : "Oyuna Basla",
+            z : "Sade zerbe",
+            ayaqz : "Ayaqdan zerbe",
+            basz : "Basdan zerbe",
+            player: 1,
+
+
+        }
+        
+
+
+
+    }       
+    
+})
